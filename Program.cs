a491b789bc2eadf23420dace304fa9f284ac9230
muhkhansa <muhammadkhansa530@gmail.com>
2022-03-26 21:41:34 +0700

    latihan object dan class

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
+
---
+++ Taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name :{0} ", DriverName);
+            Console.WriteLine("Taxi On Duty :{0} ", OnDuty);
+            Console.WriteLine("Number Passenger :{0} ", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang dalam Perjalanan", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0}Selesai mengantar penumpang", DriverName);
+        }
+    }
+}
